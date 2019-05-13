0a615f6c63dbc91eb1d906654bbc3d353bbbf75d
Mashinigami <damuron@gmail.com>
2019-05-13 07:29:11 -0300

    Adicao previsao tempo

--- index.js
+++ index.js
@@ -1,46 +1,74 @@
-const express = require('express');
-const app = express();
-const TelegramBot = require( `node-telegram-bot-api` );
-const emoji = require('node-emoji').emoji;
+const express = require('express'),
+        app = express(),
+        TelegramBot = require( `node-telegram-bot-api` ),
+        emoji = require('node-emoji').emoji,
+        bodyParser = require("body-parser"),
+        rp = require("request-promise");
 
-const TOKEN = ``;
+
+require("dotenv").config();
+
+const TOKEN = process.env.TELEGRAM_API_TOKEN;
+const telegram_url = "https://api.telegram.org/bot" + process.env.TELEGRAM_API_TOKEN +"/sendMessage";
+const openWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=";
 
 const bot = new TelegramBot(TOKEN, { polling: true });
+
+app.get('/test', (req, res) => {
+    res.send("Worked");
+});
 
 bot.on('new_chat_members', (msg) => {
     bot.sendMessage(msg.chat.id, `Hey ${msg.from.first_name}, welcome to williamrobot!! SHOW ME WHAT YOU GOT!!!!`)
 });
 
-bot.onText(/\/previsao (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+function get_forecast(city){
+    let new_url = openWeatherUrl + city+"&lang=pt&appid="+process.env.OPENWEATHER_API_KEY;
+    return rp(new_url)
+        .then((body) => {
+            return JSON.parse(body);
+        }).catch((err) => {
+            console.log(err);
+        })
+}
 
+bot.onText(/\/previsao (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
 
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
+    get_forecast(resp).then(previsoes => {
+        let previsoesFmt = 'Previsao para ';
+        previsoesFmt = previsoesFmt.concat(previsoes.city.name, "/", previsoes.city.country, "\n");
+        previsoes.list.slice(0,19).forEach(function (previsao) {
+            previsoesFmt = previsoesFmt.concat(previsao.dt_txt, " : ", previsao.weather[0].description, "\n");
+        });
+        console.log(previsoesFmt);
+        bot.sendMessage(chatId, previsoesFmt);
+    });
 });
 
-bot.on('message', function(message)
-{
+bot.on('message', function(message) {
     const chat_id = message.chat.id;
-    console.log("sending message to "+chat_id);
+
     switch (message.text) {
         case "/comandos":
-            bot.sendMessage(chat_id,"/marco\n/help");
+            console.log("sending message to "+chat_id);
+            bot.sendMessage(chat_id,"/marco\n/help\n/previsao london");
             break;
         case "/comandos@williamro_bot":
-            bot.sendMessage(chat_id,"/marco\n/help");
+            console.log("sending message to "+chat_id);
+            bot.sendMessage(chat_id,"/marco\n/help\n/previsao london");
             break;
         case "/marco":
+            console.log("sending message to "+chat_id);
             bot.sendMessage(chat_id,"Polo "+emoji.smirk);
             break;
         case "/help":
+            console.log("sending message to "+chat_id);
             bot.sendMessage(chat_id,"Error 404 help.exe not found "+emoji.see_no_evil);
             break;
         case "/teucu":
+            console.log("sending message to "+chat_id);
             bot.sendPhoto(chat_id,"https://data.whicdn.com/images/37667397/large.jpg");
             break;
     }
@@ -78,6 +106,6 @@ bot.on('update', function(message)
     console.log(6);
 });
 
-app.listen(process.env.PORT || 8181, function () {
+app.listen(3000, function () {
     console.log("Servidor Iniciado");
 });
